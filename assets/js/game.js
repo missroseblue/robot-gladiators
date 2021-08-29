72d9b054d74e47dfb9d9c26a1f88a156a16e199d
missroseblue <missroseblue@gmail.com>
2021-08-28 19:16:53 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,3 +94,37 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+//start the game when the page loads
+var startGame = function() {
+//reset player stats
+playerHealth = 100;
+playerAttack = 10;
+playerMoney = 10;
+
+for (var i = 0; i < enemyNames.length; i++) {
+
+}
+//play again
+endGame();
+};
+
+//function to end the entire game
+var endGame = function() {
+  //if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + "." );
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  //ask the player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+  if (playAgainConfirm) {
+    //restart game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
