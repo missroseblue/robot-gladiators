bd284b29553cd75cb70482768caa498c2386ee89
missroseblue <missroseblue@gmail.com>
2021-08-29 18:27:14 -0700

    update shop function to handle integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -163,25 +163,20 @@ var shop = function() {
   );
 
   // use switch case to carry out action
-  switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+  switch parseInt(shopOptionPrompt) {
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
-      window.alert('Leaving the store.');
-
-      // do nothing, so function will end
+    case 3:
+      window.alert("Leaving the store.");
       break;
     default:
-      window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
+      window.alert("You did not pick a valid option. Try again.");
       shop();
       break;
   }
@@ -259,4 +254,4 @@ console.log(enemyInfo[0]['attack']);
 /* END GAME INFORMATION / VARIABLES */
 
 /* RUN GAME */
-startGame();
+startGame();}
